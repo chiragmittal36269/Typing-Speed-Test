563130cf931199a293504d53976467a4d5446b64
Chirag Mittal <chiragmittal36269@gmail.com>
2023-10-28 18:16:10 +0530

    login and signup creation phase

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Statics from "./Components/Statics";
 import TypingInput from "./Components/TypingInput";
+import Login from "./Components/User/Login";
+import Signup from "./Components/User/Signup";
 
 const App = () => {
+    let user = localStorage.getItem("user");
+
+    console.log(user);
+
     return (
         <div className="app">
             <Header />
             <Routes>
                 <Route path="/typing-speed-test" element={<TypingInput />} />
                 <Route path="/statics" element={<Statics />} />
+                {user} ? <Route path="/login" element={<Login />} /> :{" "}
+                <Route path="/signup" element={<Signup />} />
             </Routes>
             <Footer />
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const user = localStorage.getItem("user");
+    let page = "";
+    if (user == null) {
+        page = "/signup";
+    } else {
+        page = "/login";
+    }
+
     const navigate = useNavigate();
 
     return (
@@ -17,7 +25,11 @@ const Header = () => {
                     <path d="M20 5H4c-1.1 0-1.99.9-1.99 2L2 17c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-9 3h2v2h-2V8zm0 3h2v2h-2v-2zM8 8h2v2H8V8zm0 3h2v2H8v-2zm-1 2H5v-2h2v2zm0-3H5V8h2v2zm9 7H8v-2h8v2zm0-4h-2v-2h2v2zm0-3h-2V8h2v2zm3 3h-2v-2h2v2zm0-3h-2V8h2v2z"></path>
                 </svg>
             </div>
-            <div className="right">
+            <div
+                className="right"
+                onClick={() => {
+                    navigate(page);
+                }}>
                 <svg
                     className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
                     focusable="false"
